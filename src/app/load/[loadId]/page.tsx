bd4c66ae39fb088d27cb7eb16b129ec467e68e6f
Seth Moder <seth@ideal-delivery.com>
2026-01-01 23:59:45 +0000

    The app isn't starting. Please investigate what could be wrong based on

--- src/app/load/[loadId]/page.tsx
+++ src/app/load/[loadId]/page.tsx
@@ -1,19 +1,8 @@
 
-import { DeliveryOpsDashboard } from '@/components/app/delivery-ops-dashboard';
 import { LoadDetailsPage } from '@/components/app/load-details-page';
 
-export default function LoadPage({ params }: { params: { loadId: string } }) {
-  // We will need to wrap this in the main dashboard layout
-  // For now, we render the dashboard and the detail page
-  // This will be fixed in a follow up.
+export default async function LoadPage({ params }: { params: { loadId: string } }) {
   return (
-    <>
-        <div className='hidden'>
-            <DeliveryOpsDashboard />
-        </div>
-        <LoadDetailsPage loadId={params.loadId} />
-    </>
+    <LoadDetailsPage loadId={params.loadId} />
   );
 }
-
-    
