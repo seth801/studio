9e2c7a629a7fc41f606af0080f1275092ab4054f
Seth Moder <seth@ideal-delivery.com>
2026-01-01 06:30:34 +0000

    Not liking the blue background. Lets stay closer to black.

--- src/components/app/delivery-ops-dashboard.tsx
+++ src/components/app/delivery-ops-dashboard.tsx
@@ -256,7 +256,7 @@ export function DeliveryOpsDashboard() {
           <div className="flex h-full max-h-screen flex-col gap-2">
             <div className="flex h-[60px] items-center border-b px-6">
               <a href="/" className="flex items-center gap-2 font-semibold">
-                <Logo className="h-6 w-auto text-primary" />
+                <Logo className="h-8 w-auto text-primary" />
                 <span className="group-data-[collapsible=icon]:hidden">Ideal Delivery Ops</span>
               </a>
             </div>
@@ -274,10 +274,10 @@ export function DeliveryOpsDashboard() {
                   <span className="sr-only">Toggle navigation menu</span>
                 </Button>
               </SheetTrigger>
-              <SheetContent side="left" className="sm:max-w-xs">
+              <SheetContent side="left" className="sm:max-w-xs bg-card p-0">
                 <div className="flex h-[60px] items-center border-b px-6">
                   <a href="/" className="flex items-center gap-2 font-semibold">
-                    <Logo className="h-6 w-auto text-primary" />
+                    <Logo className="h-8 w-auto text-primary" />
                     <span className="">Ideal Delivery Ops</span>
                   </a>
                 </div>
